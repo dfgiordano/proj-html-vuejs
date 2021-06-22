1bb80eed3eaaf1d7ff65d60719badd8f780972c8
dfgiordano <federico.giordano90@gmail.com>
2021-06-22 09:36:05 +0200

    menu burger quasi terminato

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,18 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 export default {
   name: 'App',
   components: {
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,37 +1,15 @@
 <template>
     <header>
-        <div class="container_nav">
-            <div class="accordion" role="tablist">
+        <div class="container-nav">
+            <div class="accordion" :class="isOpen ? 'background-color-red' : '' " role="tablist">
                 <b-card no-body class="mb-1">
                 <b-card-header header-tag="header" class="p-1" role="tab">
-                    <b-button block v-b-toggle.accordion-1 variant="info">Accordion 1</b-button>
+                    <b-button @click="clickMenuButton" class="menu-button" block v-b-toggle.accordion-1 variant="info"><i :class=" isOpen ? 'fas fa-times' : 'fas fa-bars' "></i></b-button>
                 </b-card-header>
                 <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
                     <b-card-body>
                     <b-card-text>I start opened because <code>visible</code> is <code>true</code></b-card-text>
-                    <b-card-text>{{ text }}</b-card-text>
-                    </b-card-body>
-                </b-collapse>
-                </b-card>
-
-                <b-card no-body class="mb-1">
-                <b-card-header header-tag="header" class="p-1" role="tab">
-                    <b-button block v-b-toggle.accordion-2 variant="info">Accordion 2</b-button>
-                </b-card-header>
-                <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
-                    <b-card-body>
-                    <b-card-text>{{ text }}</b-card-text>
-                    </b-card-body>
-                </b-collapse>
-                </b-card>
-
-                <b-card no-body class="mb-1">
-                <b-card-header header-tag="header" class="p-1" role="tab">
-                    <b-button block v-b-toggle.accordion-3 variant="info">Accordion 3</b-button>
-                </b-card-header>
-                <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
-                    <b-card-body>
-                    <b-card-text>{{ text }}</b-card-text>
+                    <b-card-text>stocazzo</b-card-text>
                     </b-card-body>
                 </b-collapse>
                 </b-card>
@@ -40,8 +18,8 @@
         <div class="title">
             <h1>Untold stories</h1>
             <p>There is an untold story behind every favorite song&period;</p>
-            <button class="first_btn">latest album</button>
-            <button class="second_btn">live dates</button>
+            <button class="first-btn">latest album</button>
+            <button class="second-btn">live dates</button>
         </div>
         
     </header>
@@ -51,7 +29,16 @@
 <script>
 export default {
     name: "Header",
-     
+    data: function () {
+        return {
+            isOpen : true,
+        } 
+    },
+     methods: {
+         clickMenuButton: function () {
+             this.isOpen = !this.isOpen;
+         }
+     }
 }
 
 </script>
@@ -59,7 +46,15 @@ export default {
 <style lang="scss" scoped>
   @import '../style/general.scss';
     
-    header {
+    .container-nav {
+        // width: 85%;
+        height: 900px;
+        margin: auto;
+            img {
+                height: 50px;
+            }
+    }
+    header:not(.p-1) {
         background-image: url('../assets/images/home_slider.jpg');
         background-position: 0% 60%;
         .title {
@@ -85,26 +80,46 @@ export default {
                 text-transform: uppercase;
                 border-radius: 2px;
             }
-            .first_btn {
+            .first-btn {
                 width: 200px;
                 margin-right: 10px;
                 background-color: #EA4A56;
                 border: none;
                 color: white;
             }
-            .second_btn {
+            .second-btn {
                 width: 150px;
                 background-color: transparent;
                 border: 1px solid white;
                 color: white;
+                    &:hover {
+                        background-color: white;
+                        color: #2D2E46;
+                    }
             }
     }
-.container_nav {
-    width: 85%;
-    height: 900px;
-    margin: auto;
-        img {
-            height: 50px;
+    .menu-button {
+        background-color: transparent;
+        border: none;
+    }
+    
+    .accordion {
+        div.card.mb-1 {
+            background-color: transparent;
+            header.card-header.p-1 {
+                background-color: transparent;
+                border: none;
+            }
         }
-}
+    }
+    .background-color-red {
+            background-color: #EC4758;
+        }
+        .btn-check:focus,
+        .btn-check:hover,
+        .btn-info,
+        .btn-info:focus,
+        .btn-info:hover {
+            background-color: transparent;
+        }
 </style>
